7e3c48b33a9652fcf9aab57761fa1e8ae3c83825
Michael Lewis <mjolewis@gmail.com>
2019-11-02 17:14:39 -0400

    Enhancement: 1) Added getFront() and getRear() accessor methods in LinkedQueue.java

--- Data Structures with Java/Code/src/edu/bostonuniversity/nodes/NodeTest.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/nodes/NodeTest.java
@@ -9,6 +9,10 @@ public class NodeTest {
 
         queue.add(7);
         queue.add(9);
+        System.out.println("Front node is: " + queue.getFront().getData());
+        System.out.println("Rear node is: " + queue.getRear().getData());
+        queue.getFront().setData(77777);
+        System.out.println("Front node is: " + queue.getFront().getData());
         System.out.println("Empty Queue: " + queue.isEmpty());
         System.out.println("Size of Queue: " + queue.size());
         System.out.println("Removing front item: " + queue.remove());
