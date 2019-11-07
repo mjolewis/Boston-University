cfeab566f851a66e5cab456070e73f74b186daa1
Michael Lewis <mjolewis@gmail.com>
2019-11-07 09:46:58 -0500

    Including the homework package a shell for the Recurse.java file

---
+++ Data Structures with Java/Code/src/edu/bostonuniversity/homework/Recurse.java
@@ -0,0 +1,19 @@
+// File Recurse.java from the package edu.bostonuniversity.homework
+
+/**********************************************************************************************************************
+ * I recurse, therefore I recurse. This program illustrates the concept of recursion by writing the output of a
+ * recursive call using System.out.println().
+ *
+ * @note
+ *  1) A deep recursive call might lead to a StackOverflowError.
+ *  2) A recursive call without a base case and a variant condition will result in infinite recursion (until a
+ *     StackOverflowError occurs, but our programming has prevented this type of error.
+ *
+ * @author mlewis
+ * @version Nov 7, 2019
+ *********************************************************************************************************************/
+
+package edu.bostonuniversity.homework;
+
+public class Recurse {
+}
