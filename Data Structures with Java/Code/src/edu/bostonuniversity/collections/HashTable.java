2f5213b1a499bba390d50ed0cb77e975a1aec775
Michael Lewis <mjolewis@gmail.com>
2019-11-24 09:59:58 -0500

    Specification and implementation of default and overloaded constructors in HashTable.java

---
+++ Data Structures with Java/Code/src/edu/bostonuniversity/collections/HashTable.java
@@ -0,0 +1,66 @@
+// File HashTable.java from the package edu.bostonuniversity.collections
+
+package edu.bostonuniversity.collections;
+
+/**********************************************************************************************************************
+ * A HashTable is a collection used to map keys to values. Any non-null object can be used as a key or as a value.
+ * Traditionally, the class is implemented using Java Generics but, our implementation will make use of integer keys
+ * and String values.
+ *
+ * @note
+ *  1. A HashTable's performance is affected by it's initial capacity, which should always be a twin prime to optimize
+ *     performance.
+ *  2. This implementation using Open Addressing to resolve collisions. Meaning that if multiple values are hashed to
+ *     the same bucket, then we search for the next open bucket in a sequential manner.
+ *
+ * @author mlewis
+ * @version November 24, 2019
+ *********************************************************************************************************************/
+
+public class HashTable {
+    // Invariant of the HashTable class.
+    // 1. The number of elements in the table is in the instance variable size.
+    // 2. The proper bucket for any given value with a specified key is at hash(key). However, if a collision occurs
+    //    then we use open addressing to find the next available bucket. Whenever an open bucket is found, we store
+    //    the value in data[index] and the value's key in key[index].
+    // 3. The instance variable hasBeenUsed indicates whether or not data[index] has ever been used.
+    private int size;
+    private int[] keys;
+    private String[] data;
+    private boolean[] hasBeenUsed;
+
+    /**
+     * public HashTable()
+     * Initialize an empty HashTable with an initial capacity of 103, which is a twin prime.
+     * @postcondition
+     *  An empty HashTable with an initial capacity of 103 has been initialized.
+     * @exception OutOfMemoryError
+     *  Indicates insufficient memory for this HashTable
+     */
+    public HashTable() {
+        size = 0;
+        keys = new int[103];
+        data = new String[103];
+        hasBeenUsed = new boolean[103];
+    }
+
+    /**
+     * public HashTable(int capacity)
+     * Initialize a HashTable with the given initial capacity.
+     * @param capacity
+     *  The initial capacity of the HashTable.
+     * @postcondition
+     *  A HashTable with an initial capacity has been initialized.
+     * @exception OutOfMemoryError
+     *  Indicates insufficient memory for this HashTable
+     */
+    public HashTable(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity cannot be negative: " + capacity);
+        }
+        size = 0;
+        keys = new int[capacity];
+        data = new String[capacity];
+        hasBeenUsed = new boolean[capacity];
+    }
+}
