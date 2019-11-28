fe89d05353a6ceec93864925a3ba63804149f449
Michael Lewis <mjolewis@gmail.com>
2019-11-28 12:33:30 -0500

    Initial commit for a skeleton of the TextAnalyzerMain.java class

---
+++ Data Structures with Java/Code/src/edu/bostonuniversity/projects/TextAnalyzerMain.java
@@ -0,0 +1,14 @@
+// File TextAnalyzerMain.java from the package edu.bostonuniversity.projects
+
+package edu.bostonuniversity.projects;
+
+/**********************************************************************************************************************
+ * TextAnalyzerMain initializes a TextAnalyzer object that is used to read in a body of text, insert the text into a
+ * binary tree, and answer a series of questions about the body of text.
+ *
+ * @author mlewis
+ * @version November 28, 2019
+ *********************************************************************************************************************/
+
+public class TextAnalyzerMain {
+}
