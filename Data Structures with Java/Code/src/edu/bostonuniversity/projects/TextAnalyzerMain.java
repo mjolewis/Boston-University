4bbdaeee55b3ca18ae5e8efad846adb87ce046b8
Michael Lewis <mjolewis@gmail.com>
2019-11-28 12:45:22 -0500

    Implementation of main() method in TextAnalyzerMain.java. Implementation is incomplete and will be worked on later.

--- Data Structures with Java/Code/src/edu/bostonuniversity/projects/TextAnalyzerMain.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/projects/TextAnalyzerMain.java
@@ -2,6 +2,8 @@
 
 package edu.bostonuniversity.projects;
 
+import edu.bu.met.cs342a1.TextParser;
+
 /**********************************************************************************************************************
  * TextAnalyzerMain initializes a TextAnalyzer object that is used to read in a body of text, insert the text into a
  * binary tree, and answer a series of questions about the body of text.
@@ -11,4 +13,20 @@ package edu.bostonuniversity.projects;
  *********************************************************************************************************************/
 
 public class TextAnalyzerMain {
+    public static void main(String[] args) {
+        String FILE_NAME = ;
+        String word;
+        TextAnalyzer textAnalyzer = new TextAnalyzer();
+        TextParser parser = new TextParser();
+        boolean fileExists;
+
+        fileExists = parser.openFile(FILE_NAME);
+        if (fileExists) {
+            word = parser.getNextWord();
+            while (word != null) {
+                textAnalyzer.add(textAnalyzer.getRoot(), word);
+                word = parser.getNextWord();
+            }
+        }
+    }
 }
