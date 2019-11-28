32d90104a20340911a36ccb8ab82014337bc11d1
Michael Lewis <mjolewis@gmail.com>
2019-11-27 19:04:56 -0500

    Pseudocode for a generic Binary Tree

---
+++ Data Structures with Java/Code/src/edu/bostonuniversity/projects/BinaryTree.java
@@ -0,0 +1,13 @@
+// File BinaryTree.java from the package edu.bostonuniversity.projects
+
+package edu.bostonuniversity.projects;
+
+/**********************************************************************************************************************
+ * A BinaryTree class
+ *
+ * @author mlewis
+ * @version November 26, 2019
+ *********************************************************************************************************************/
+
+public class BinaryTree {
+}
