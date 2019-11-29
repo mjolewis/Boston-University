1baf6beb314860ee20b073f58c8592e5c5ac1579
Michael Lewis <mjolewis@gmail.com>
2019-11-29 10:16:25 -0500

    Pseudocode, specification, and implementation of TextAnalyzerMain methods.

--- Data Structures with Java/Code/src/edu/bostonuniversity/projects/TextAnalyzerMain.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/projects/TextAnalyzerMain.java
@@ -13,13 +13,51 @@ import edu.bu.met.cs342a1.TextParser;
  *********************************************************************************************************************/
 
 public class TextAnalyzerMain {
-    public static void main(String[] args) {
-        String FILE_NAME = "/Users/mlewis/Downloads/Dracula.txt";
-        TextAnalyzer<String> textAnalyzer = new TextAnalyzer<>();
-        TextParser parser = new TextParser();
-        boolean fileExists;
+    // Invariant of the TextAnalyzerMain class.
+    //  1. The instance variable FILE_NAME is an absolute pathname to a text file on your local system.
+    //  2. The instance variable textAnalyzer is a TextAnalyzer object used to build a binary tree from the text from
+    //     the text stored in FILE_NAME.
+    //  3. The instance variable parser is a TextParser object used to read in a text file stored at FILE_NAME.
+    //  4. The instance variable fileExists is a boolean object used to confirm whether or not the file stored in
+    //     FILE_NAME exists and has been opened for processing.
+    private static final String FILE_NAME = "/Users/mlewis/Downloads/Dracula.txt";
+    private TextAnalyzer<String> textAnalyzer;
+    private TextParser parser;
+    private boolean fileExists;
 
-        fileExists = parser.openFile(FILE_NAME);
-        if (fileExists) { textAnalyzer.parse(parser); }
+    /**
+     * public static void main(String[] args)
+     * The main entry point for the TextAnalyzer.java class
+     * @param args
+     *  The required signature for Java's main method.
+     */
+    public static void main(String[] args) {
+        TextAnalyzerMain start = new TextAnalyzerMain();
+        start.openFile(FILE_NAME);
+    }
+
+    /**
+     * public void buildTree(TextParser file)
+     * Builds a binary search tree from the text stored in the specified file.
+     * @postcondition
+     *  A binary search tree has been built using the text stored in the specified file.
+     */
+    public void buildTree(TextParser file) {
+        textAnalyzer = new TextAnalyzer<>();
+        textAnalyzer.parse(parser);
+
+    }
+
+    /**
+     * public void openFIle(String fileName)
+     * @param fileName
+     *  The absolute pathname for a file on your local system.
+     * @postcondition
+     *  If the file exists, then it has been opened.
+     */
+    public void openFile(String fileName) {
+        parser = new TextParser();
+        fileExists = parser.openFile(fileName);
+        if (fileExists) { buildTree(parser); }
     }
 }
