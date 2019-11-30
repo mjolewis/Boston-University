a793d16dcd0a3e9f8f4217127155014b75c6322d
Michael Lewis <mjolewis@gmail.com>
2019-11-29 20:22:15 -0500

    Code cleaning.

--- Data Structures with Java/Code/src/edu/bostonuniversity/projects/BinarySearchTreeMain.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/projects/BinarySearchTreeMain.java
@@ -1,4 +1,4 @@
-// File TextAnalyzerMain.java from the package edu.bostonuniversity.projects
+// File BinarySearchTreeMain.java from the package edu.bostonuniversity.projects
 
 package edu.bostonuniversity.projects;
 
@@ -6,25 +6,22 @@ import edu.bostonuniversity.nodes.BTNode;
 import edu.bu.met.cs342a1.TextParser;
 
 /**********************************************************************************************************************
- * TextAnalyzerMain initializes a TextAnalyzer object that is used to read in a body of text, insert the text into a
- * binary tree, and answer a series of questions about the body of text.
+ * BinarySearchTreeMain initializes a Binary Search Tree object that is used to read in a body of text, insert the text
+ * into a Binary Search Tree, and answer a series of questions about the body of text.
  *
  * @author mlewis
  * @version November 28, 2019
  *********************************************************************************************************************/
 
-public class TextAnalyzerMain {
-    // Invariant of the TextAnalyzerMain class.
+public class BinarySearchTreeMain {
+    // Invariant of the BinarySearchTreeMain class.
     //  1. The instance variable FILE_NAME is an absolute pathname to a text file on your local system.
-    //  2. The instance variable textAnalyzer is a TextAnalyzer object used to build a binary tree from the text from
-    //     the text stored in FILE_NAME.
+    //  2. The instance variable binarySearchTree is a binarySearchTree object used to build a Binary Search Tree
+    //     the text from the text stored in FILE_NAME.
     //  3. The instance variable parser is a TextParser object used to read in a text file stored at FILE_NAME.
-    //  4. The instance variable fileExists is a boolean object used to confirm whether or not the file stored in
-    //     FILE_NAME exists and has been opened for processing.
     private static final String FILE_NAME = "/Users/mlewis/Downloads/Dracula.txt";
     private BinarySearchTree<String> binarySearchTree;
     private TextParser parser;
-    private boolean fileExists;
 
     /**
      * public void buildTree(TextParser file)
@@ -40,12 +37,12 @@ public class TextAnalyzerMain {
 
     /**
      * public static void main(String[] args)
-     * The main entry point for the TextAnalyzer.java class
+     * The main entry point for the BinarySearchTreeMain.java class
      * @param args
      *  The required signature for Java's main method.
      */
     public static void main(String[] args) {
-        TextAnalyzerMain start = new TextAnalyzerMain();
+        BinarySearchTreeMain start = new BinarySearchTreeMain();
         start.openFile(FILE_NAME);
     }
 
@@ -58,7 +55,7 @@ public class TextAnalyzerMain {
      */
     public void openFile(String fileName) {
         parser = new TextParser();
-        fileExists = parser.openFile(fileName);
+        boolean fileExists = parser.openFile(fileName);
         if (fileExists) { buildTree(parser); }
     }
 
