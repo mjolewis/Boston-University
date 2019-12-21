d9c0b551c3bc90d5251a894ebd1dd4dfdfde4146
Michael Lewis <mjolewis@gmail.com>
2019-12-21 13:42:09 -0500

    Fix: correct partition method
    -swap elements in right side correctly.

--- Data Structures with Java/Code/src/edu/bostonuniversity/sorts/Sort.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/sorts/Sort.java
@@ -16,32 +16,29 @@ public class Sort {
      * Utility method used to sort the specified array in O(n log n) time complexity.
      * @param data
      *  The array to be sorted.
-     * @param first
+     * @param start
      *  The first index in the data array.
-     * @param n
+     * @param end
      *  The length of the data array.
      * @precondition
      *  n is greater than 1, indicating that the array contains data.
-     * @return int[]
-     *  The sorted data array.
      * @postcondition
      *  The data array has been sorted from smallest to largest.
      */
-    public static int[] quickSort(int[] data, int first, int n) {
+    public static void quickSort(int[] data, int start, int end) {
         int pivotIndex;
         int n1;
         int n2;
 
-        if (n > 1) {
-            pivotIndex = partition(data, first, n);
+        if (end > 1) {
+            pivotIndex = partition(data, start, end);
 
-            n1 = pivotIndex - first;
-            n2 = n - n1 - 1;
+            n1 = pivotIndex - start;
+            n2 = end - n1 - 1;
 
-            quickSort(data, first, n1);
+            quickSort(data, start, n1);
             quickSort(data, pivotIndex + 1, n2);
         }
-        return data;
     }
 
     /**
@@ -49,16 +46,16 @@ public class Sort {
      * Helper method that returns a pivot index used by the quickSort method.
      * @param data
      *  The array to be sorted.
-     * @param first
+     * @param start
      *  The first index in the data array.
-     * @param n
+     * @param end
      *  The length of the data array
      * @return int
      *  The pivot index.
      */
-    private static int partition(int[] data, int first, int n) {
+    private static int partition(int[] data, int start, int end) {
+        int pivotIndex;
         int pivot;
-        int pivotElement;
         int randomIndex1;
         int randomIndex2;
         int randomIndex3;
@@ -68,27 +65,27 @@ public class Sort {
 
         // Find three random numbers within the bounds of the data array and use the values at those indexes to
         // determine a pivotElement.
-        randomIndex1 = (int) (Math.random() * n);
-        randomIndex2 = (int) (Math.random() * n);
-        randomIndex3 = (int) (Math.random() * n);
+        randomIndex1 = start + (int) (Math.random() * end);
+        randomIndex2 = start + (int) (Math.random() * end);
+        randomIndex3 = start + (int) (Math.random() * end);
 
-        pivotElement = Math.max(Math.min(data[randomIndex1], data[randomIndex2]), Math.min(Math.max(data[randomIndex1],
+        pivot = Math.max(Math.min(data[randomIndex1], data[randomIndex2]), Math.min(Math.max(data[randomIndex1],
                 data[randomIndex2]), data[randomIndex3]));
 
-        // Find the pivot index;
-        if (data[randomIndex1] == pivotElement) {
-            pivot = randomIndex1;
-        } else if (data[randomIndex2] == pivotElement) {
-            pivot = randomIndex2;
+        // Find the pivot index
+        if (data[randomIndex1] == pivot) {
+            pivotIndex = randomIndex1;
+        } else if (data[randomIndex2] == pivot) {
+            pivotIndex = randomIndex2;
         } else {
-            pivot = randomIndex3;
+            pivotIndex = randomIndex3;
         }
 
-        tooBigIndex = 0;
-        tooSmallIndex = n - 1;
-        while (tooBigIndex <= tooSmallIndex) {
-            while (tooBigIndex <= n && data[tooBigIndex] <= pivotElement) { tooBigIndex++; }
-            while (data[tooSmallIndex] > pivotElement) { tooSmallIndex--; }
+        tooBigIndex = start;
+        tooSmallIndex = start + end - 1;
+        while (tooBigIndex < tooSmallIndex) {
+            while (data[tooBigIndex] < pivot) { tooBigIndex++; }
+            while (data[tooSmallIndex] > pivot) { tooSmallIndex--; }
 
             // Swap the elements so that they are in the correct partition of the array.
             if (tooBigIndex < tooSmallIndex) {
@@ -98,9 +95,11 @@ public class Sort {
             }
         }
 
-        temp = data[tooSmallIndex];
-        data[tooSmallIndex] = data[pivot];
-        data[pivot] = temp;
+        if (data[tooSmallIndex] < pivot) {
+            temp = data[tooSmallIndex];
+            data[tooSmallIndex] = pivot;
+            data[pivotIndex] = temp;
+        }
 
         return tooSmallIndex;
     }
