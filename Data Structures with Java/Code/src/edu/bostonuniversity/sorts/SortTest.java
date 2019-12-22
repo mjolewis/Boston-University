6322f8aca077d53a927c1a7f7dac273e3eee9103
Michael Lewis <mjolewis@gmail.com>
2019-12-22 17:01:15 -0500

    Chore: regular code maintenance
    - simplify return statement in partition method.
    
    - simplify documentation.

--- Data Structures with Java/Code/src/edu/bostonuniversity/sorts/SortTest.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/sorts/SortTest.java
@@ -7,24 +7,21 @@ public class SortTest {
     }
 
     public void doIt() {
-        //int[] data = {5, 4, 3, 2, 1, 0, -1, -2, 88, 234, 583, 29, 40, 92, 91, 498, 487, 29, 458, 58, 11, 2354, 53};
-        //int[] data = {40, 20, 40, 40, 40, 40, 8, 9, 9, 8, 40};
-        int[] data = new int[1000000];
+        int[] data = new int[15];
         for (int i = 0; i < data.length; i++) {
-            int j = (int) (Math.random() * 100000);
+            int j = (int) (Math.random() * 50);
             data[i] = j;
         }
 
         System.out.println("Starting sort");
         long start = System.nanoTime();
 
-        //S.qSort(data);
         Sort.quickSort(data);
 
         long end = System.nanoTime();
         long time = end - start;
 
-        //for (int datum : data) { System.out.print(datum + " "); }
+        for (int datum : data) { System.out.print(datum + " "); }
 
         System.out.println("\nTook: " + time / 1000000000.0 + " seconds");
     }
