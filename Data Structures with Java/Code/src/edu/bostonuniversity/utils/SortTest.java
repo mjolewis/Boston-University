fbc48e679e70de952d1eb6dd641b434af983d624
Michael Lewis <mjolewis@gmail.com>
2019-12-23 21:08:30 -0500

    Chore: regular maintenance
    - update utils package naming conventions.

--- Data Structures with Java/Code/src/edu/bostonuniversity/utils/SortTest.java
+++ Data Structures with Java/Code/src/edu/bostonuniversity/utils/SortTest.java
@@ -1,4 +1,4 @@
-package edu.bostonuniversity.sorts;
+package edu.bostonuniversity.utils;
 
 public class SortTest {
     public static void main(String[] args) {
@@ -7,16 +7,17 @@ public class SortTest {
     }
 
     public void doIt() {
-        int[] data = new int[15];
+        int[] data = new int[20];
         for (int i = 0; i < data.length; i++) {
-            int j = (int) (Math.random() * 50);
+            int j = (int) (Math.random() * 100);
             data[i] = j;
         }
 
         System.out.println("Starting sort");
         long start = System.nanoTime();
 
-        Sort.quickSort(data);
+        Quicksort.quicksort(data);
+        //S.quicksort(data);
 
         long end = System.nanoTime();
         long time = end - start;
